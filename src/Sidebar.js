c19a387f575300d66b7688ea826f13a79e72599d
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-12 09:44:54 -0400

    Adjust CSS

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+	render() {
+		return (
+			<div class="Sidebar">
+				Sidebar
+			</div>
+		);
+	}
+}
+
+export default Sidebar;
