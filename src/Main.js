8242891f1115abb34ca03c01095a1c590447f084
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-12 09:51:50 -0400

    style Main

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import Sidebar from './Sidebar.js'
+import Chat from './Chat.js'
 
 class Main extends Component {
 	render() {
 		return (
-			<div className="Main">
-				Main
+			<div className="Main" style={styles}>
+				<Sidebar />	
+				<Chat />
 			</div>
 		);
 	}
 }
+
+const styles = {
+	display: 'flex',
+	alignItems: 'stretch',
+	height: '100vh'
+};
 
 export default Main;
