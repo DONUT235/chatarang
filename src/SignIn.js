a19f3b70db475798b5d1268b93db88213e7cb6cc
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-12 21:30:42 -0400

    Add prompt to SignIn

--- src/SignIn.js
+++ src/SignIn.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 class SignIn extends Component {
 	render() {
 		return (
-			<form onSubmit={this.props.logIn}>
-				<input type="text" placeholder="Username" name="username"/>
-				<input type="submit" value="Log In"/>
-			</form>
+			<div>
+				<h1>You are not logged in.</h1>
+				<form onSubmit={this.props.logIn}>
+					<input type="text" placeholder="Username" name="username"/>
+					<input type="submit" value="Log In"/>
+				</form>
+			</div>
 		);
 	}
 }
