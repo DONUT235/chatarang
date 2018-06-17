3498ee6556a198bd927254ecf42ed2ebf165ab5e
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-17 18:37:13 -0400

    Improve room form appearance and validate room names

--- src/RoomForm.js
+++ src/RoomForm.js
@@ -6,22 +6,31 @@ class RoomForm extends Component {
 		this.state = {
 			name: '',
 			description: '',
+			error: '',
 		};
 	}
-	handleChange = name => (
-		ev => {
+	handleChange = ev => {
 			const o = {};
-			o[name] = ev.target.value;
+			o[ev.target.name] = ev.target.value;
 			this.setState(o);
-		}
-	);
+	};
+	cancel = () => {
+		this.props.toggleForm();
+		this.setState({error: ''});
+	};
 	handleSubmit = ev => {
 		ev.preventDefault();
-		this.props.addChannel(
-			ev.target.name.value,
-			ev.target.description.value
-		);
-		this.props.toggleForm();
+		if((/^[^[\].$#\/]+$/).test(this.state.name)) {
+			this.props.addChannel(
+				this.state.name,
+				this.state.description
+			);
+			this.cancel();
+		} else if(this.state.name.length > 0) {
+			this.setState({error: 'Room names may not contain [].$#/'});
+		} else {
+			this.setState({error: 'Room name must not be empty'});
+		}
 	};
 	render() {
 		return (
@@ -29,19 +38,34 @@ class RoomForm extends Component {
 				<form onSubmit={this.handleSubmit}>
 					<p>
 						<label>Room name</label>
-						<input type="text" name="name"/>
+						<input type="text" name="name" onChange={this.handleChange}/>
 					</p>
 					<p>
 						<label>Description</label>
-						<input type="text" name="description"/>
+						<input type="text" name="description" onChange={this.handleChange}/>
 					</p>
 					<div>
-						<button type="button" onClick={this.props.toggleForm}>Cancel</button>
-						<button type="submit">Create Room</button>
+						<button type="submit" style={style.button}>Create Room</button>
+						<button type="button" onClick={this.cancel} style={style.button}>Cancel</button>
 					</div>
 				</form>
+				<span style={style.error}>{this.state.error}</span>
 			</div>
 		);
+	}
+}
+
+const style = {
+	button: {
+		borderRadius: '0.8rem',
+		fontSize: '1rem',
+		borderWidth:  '0px',
+		marginRight: '8px',
+		backgroundColor: 'rgb(6, 13, 45)',
+		color: 'white',
+	},
+	error: {
+		color: 'red',
 	}
 }
 
